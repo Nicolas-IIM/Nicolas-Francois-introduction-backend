ff926b34e0dfbab26315ba09f0d66e2aab5b95f7
Nicolas-frnc <174860069+Nicolas-frnc@users.noreply.github.com>
2025-04-05 12:15:24 +0200

    Add functions to calculate and display average

--- calculer_la_moyenne/calcule_moyenne.php
+++ calculer_la_moyenne/calcule_moyenne.php
@@ -1,1 +1,29 @@
 <?php
+
+$note1 = 1;
+$note2 = 2;
+$note3 = 15;
+$nom = "Alice";
+
+function calculerMoyenne(){
+    global $note1, $note2, $note3;
+    $moyenne = ($note1 + $note2 + $note3) / 3;
+    return $moyenne;
+
+}
+echo calculerMoyenne();
+echo "<br>";
+
+function afficherResultat(){
+    global $nom, $moyenne;
+    if ($moyenne <= 9){
+        echo "La moyenne de $nom est de $moyenne est elle est insuffisante";
+
+    }
+    else if ($moyenne <= 10){
+        echo "La moyenne de $nom est de $moyenne est elle est suffisante";
+    }
+
+}
+
+echo afficherResultat();

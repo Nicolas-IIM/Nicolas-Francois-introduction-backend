0c529360478826162a6200d6f11b17775748b07c
Nicolas-frnc <174860069+Nicolas-frnc@users.noreply.github.com>
2025-04-06 15:45:23 +0200

    Refactor calcule_moyenne.php to add comments

--- calculer_la_moyenne/calcule_moyenne.php
+++ calculer_la_moyenne/calcule_moyenne.php
@@ -3,27 +3,27 @@
 $note1 = 1;
 $note2 = 2;
 $note3 = 15;
-$nom = "Alice";
+$nom = "Alice"; //les valeurs sont defini ici mais on peut les changer
 
-function calculerMoyenne(){
+function calculerMoyenne(){ // fonction qui calcule la moyenne
     global $note1, $note2, $note3;
     $moyenne = ($note1 + $note2 + $note3) / 3;
     return $moyenne;
 
 }
 echo calculerMoyenne();
-echo "<br>";
+echo "<br>"; // saut de ligne vide (pour le visuel)
 
-function afficherResultat(){
+function afficherResultat(){ // fonction qui affiche le resultat
     global $nom, $moyenne;
-    if ($moyenne <= 9){
-        echo "La moyenne de $nom est de $moyenne est elle est insuffisante";
+    if ($moyenne <= 9){ // boucle avec condition
+        echo "La moyenne de $nom est de $moyenne est elle est insuffisante"; // affiche si 9 ou moins
 
     }
-    else if ($moyenne <= 10){
-        echo "La moyenne de $nom est de $moyenne est elle est suffisante";
+    else if ($moyenne >= 10){
+        echo "La moyenne de $nom est de $moyenne est elle est suffisante"; // affiche si 10 ou plus
     }
 
 }
 
-echo afficherResultat();
+echo afficherResultat(); // appel de la fonction pour l'afficher
